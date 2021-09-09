50c93f0ebdb6f605d95ca94e80d9a36f42f0e412
Saurabh upreti <saurabh.urt@gmail.com>
2021-09-09 16:41:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Props from "./Props";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" container App">
+      <h2 className="my-3">PROPS EXAMPLE</h2>
+      <hr/>
+      <div className="container props">
+        <Props heading="PROPS" />
+      </div>
     </div>
   );
 }
---
+++ src/Props.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Props (props) {
+    return (
+        <>
+          <div className="containe">
+            <div className="container">
+              <h1>{props.heading}</h1>
+            </div>
+          </div>
+        </>
+    )
+}
